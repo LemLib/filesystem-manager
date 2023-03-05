8ad4f335f9a456f185dd0af3370a314bb6730e1c
Exortions <exortionsmc@gmail.com>
2023-03-04 20:10:58 -0800

    update to 0.2.0

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,51 @@
 #include <filesystem>
 #include <functional>
 #include <iostream>
+#include <stdio.h>
 #include <string>
 #include <map>
 
 namespace fs = std::filesystem;
 
-const std::string VERSION = "0.1.0";
+const std::string VERSION = "0.2.0";
+
+const std::string INDEX_FILE = "/usd/lemlib.index";
+
+std::vector<std::string> split(std::string s, std::string delimiter) {
+    size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+    std::string token;
+    std::vector<std::string> res;
+
+    while ((pos_end = s.find(delimiter, pos_start)) != std::string::npos) {
+        token = s.substr (pos_start, pos_end - pos_start);
+        pos_start = pos_end + delim_len;
+        res.push_back (token);
+    }
+
+    res.push_back (s.substr (pos_start));
+    return res;
+}
+
+void writeToIndexFile(std::string path) {
+	std::ofstream file(INDEX_FILE, std::ios_base::app);
+	
+	file << "/usd/" << path << std::endl;
+
+	file.close();
+}
+
+std::vector<std::string> readIndexFile() {
+	std::vector<std::string> files;
+
+	std::ifstream file(INDEX_FILE);
+	std::string line;
+
+	while (std::getline(file, line)) {
+		files.push_back(line);
+	}
+
+	return files;
+}
 
 bool version(std::string args) {
 	std::string msg = "LemLib file-system manager version: " + VERSION;
@@ -21,18 +60,17 @@ bool version(std::string args) {
 	return true;
 }
 
-// TODO: doesn't work
 bool ls(std::string args) {
-	std::cout << "Listing files..." << std::endl;
+	std::vector<std::string> files = readIndexFile();
 
-	for (const auto & entry : fs::directory_iterator("/usd/")) std::cout << entry.path() << std::endl; 
+	for (std::string file : files) {
+		std::cout << file << std::endl;
+	}
 
 	return true;
 }
 
 bool cat(std::string args) {
-	std::cout << "Reading file..." << std::endl;
-
 	std::ifstream file("/usd/" + args);
 	std::string line;
 
@@ -45,24 +83,24 @@ bool cat(std::string args) {
 
 // TODO: doesn't work
 bool rm(std::string args) {
-	std::cout << "Removing file..." << std::endl;
-
 	fs::remove("/usd/" + args);
 
 	return true;
 }
 
 bool write(std::string args) {
-	std::cout << "Writing file..." << std::endl;
-
 	json::JSON data = json::JSON::Load(args);
 
-	std::cout << "Writing to file: " << data["file"].ToString() << std::endl;
+	std::ofstream output;
+	output.open("/usd/" + data["file"].ToString());
 
-	std::cout << "JSON data: '" << data << "'" << std::endl;
+	for (json::JSON j : data["data"].ArrayRange()) {
+		output << j.ToString() << std::endl;
+	}
 
-	std::ofstream file("/usd/" + data["file"].ToString());
-	file << data["data"].ToString();
+	output.close();
+
+	writeToIndexFile(data["file"].ToString());
 
 	return true;
 }
@@ -108,13 +146,4 @@ void initialize() {
 			std::cout << "Unknown command: " << command << std::endl;
 		}
 	}
-}
-
-/**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
- */
-void disabled() {
-
 }
